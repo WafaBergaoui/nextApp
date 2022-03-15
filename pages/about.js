90359246a5b3042f1bd13f359c1382fae15db2b3
githubMahmoudHentati <hentati.mahmoud93@gmail.com>
2022-03-15 17:37:16 +0100

    add SAAS

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../styles/About.module.scss'
+
+const About = () => {
+    return (
+        <div className={styles.highlightscss}>
+            About Page
+        </div>
+    )
+}
+
+export default About
